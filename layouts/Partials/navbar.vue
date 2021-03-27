4e4de45d7e143c9fcf956151b3f0ad857a1daf1b
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-27 14:08:46 +0430

    create navbar

---
+++ layouts/Partials/navbar.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-card
+    class="mx-auto overflow-hidden"
+    height="auto"
+    width="auto"
+  >
+    <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+    <v-app-bar
+      color="deep-purple accent-4"
+      dark
+      prominent
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title style="text-align: center">Caspian Transplantation</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      top
+      temporary
+      width="25%"
+      style="background: aliceblue"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+
+          <template v-if="!loggedIn">
+            <v-list-item>
+              <v-list-item-title style="text-align: center" href="">ثبت نام</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title style="text-align: center" href="">
+                <nuxt-link :to="{ name: 'login' }" style="text-align: center">
+                  ورود
+                </nuxt-link>
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title style="text-align: center" href="">کاسپین</v-list-item-title>
+            </v-list-item>
+          </template>
+
+          <template v-if="loggedIn">
+            <v-list-item>
+              <v-list-item-title style="text-align: center" href="">نام کاربری</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title style="text-align: center" href="">کاسپین</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title style="text-align: center" @click.prevent="logout">خروج</v-list-item-title>
+            </v-list-item>
+          </template>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-card-text style="text-align: center">
+      Caspian
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+  mounted() {
+    console.log("loggedIn",this.loggedIn)
+  },
+  methods: {
+    logout() {
+      this.$auth.logout();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
