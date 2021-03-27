53a0b05dbd6582a2bfd644f70003867c4cd40ae9
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-27 14:10:09 +0430

    create global variable

---
+++ plugins/mixins/user.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+
+const User = {
+  install (Vue, Options) {
+    Vue.mixin({
+      computed: {
+        ...mapGetters({
+          user: 'user',
+          loggedIn: 'loggedIn'
+        })
+      }
+    })
+  }
+}
+
+Vue.use(User);
