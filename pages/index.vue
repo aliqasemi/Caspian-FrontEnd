a1716acfb5e0d5f4d0ce212d51801af0fa4f2065
Ali Qasemi <qasemi4245qq@gmail.com>
2021-03-27 10:58:14 +0430

    delete unused file

--- pages/index.vue
+++ pages/index.vue
@@ -77,13 +77,7 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
 }
 </script>
